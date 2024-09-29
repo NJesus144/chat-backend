fe7fa080ce53e7dd6c3c794d228546928161653a
NJesus144 <nalbert-edm@outlook.com>
2024-09-29 12:57:41 -0300

    Refactor auth controller and update user DTO filenames

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
+/* eslint-disable prettier/prettier */
 import { Body, Controller, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
-import { RegisterUserDto } from '../users/dto/register.user.dto'
+import { RegisterUserDto } from '../users/dto/register-user.dto'
 import { User } from '../schemas/user.schema'
+import { LoginUserDto } from '../users/dto/login-user.dto'
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +12,10 @@ export class AuthController {
   @Post('register')
   async register(@Body() registerUserDto: RegisterUserDto): Promise<User> {
     return this.authService.register(registerUserDto)
+  }
+
+  @Post('login')
+  async login(@Body() loginUseDto: LoginUserDto): Promise<{ access_token: string }> {
+    return this.authService.login(loginUseDto)
   }
 }
