311c7564a018d1b8fd979bb0467b306d6c0c4be8
NJesus144 <nalbert-edm@outlook.com>
2024-09-29 12:58:23 -0300

    Refactor user schema and add auto-generated id field

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,10 +1,13 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { Document } from 'mongoose'
+import mongoose, { Document } from 'mongoose'
 
 export type UserDocument = User & Document
 
 @Schema()
 export class User {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, auto: true })
+  id: string
+
   @Prop({ required: true, unique: true })
   username: string
 
