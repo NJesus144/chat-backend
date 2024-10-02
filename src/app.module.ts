1d1c1a84d6ab441945e5037fded3f30ac09a8902
NJesus144 <nalbert-edm@outlook.com>
2024-10-02 10:27:11 -0300

    create database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,15 @@
 import { Module } from '@nestjs/common'
-import { MongooseModule } from '@nestjs/mongoose'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './users/users.module'
-import { ConfigModule, ConfigService } from '@nestjs/config'
-import { PassportModule } from '@nestjs/passport'
+import { ConfigModule } from '@nestjs/config'
+import { DatabaseModule } from './database/database.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGODB_URI'),
-      }),
-      inject: [ConfigService],
-    }),
+    DatabaseModule,
     AuthModule,
     UserModule,
   ],
