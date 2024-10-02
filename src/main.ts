f45990ce498501cd5a10cecd4e160cfbc226bc1a
NJesus144 <nalbert-edm@outlook.com>
2024-10-02 10:58:59 -0300

    create http excpetion filters and add to main

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { HttpExceptionFilter } from './common/filters/http-exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
-
-  // app.enableCors({
-  //   origin: 'http://localhost:3000',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   allowedHeaders: 'Content-Type, Accept, Authorization,',
-  //   credentials: true,
-  // })
-
+  app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(3001)
 }
 bootstrap()
