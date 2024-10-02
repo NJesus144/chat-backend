b95984589fb5b4a4bf886dc813dfad9d3c1dc4f3
NJesus144 <nalbert-edm@outlook.com>
2024-10-02 10:37:41 -0300

    Refactor UsersService to use Mongoose and bcrypt for user creation and retrieval

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,24 +1,41 @@
-import { Injectable } from '@nestjs/common'
-
-// This should be a real class/interface representing a user entity
-export type User = any
+import { RegisterUserDto } from './dto/register-user.dto'
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+import { User, UserDocument } from '../schemas/user.schema'
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess',
-    },
-  ]
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async createUser(registerUserDto: RegisterUserDto): Promise<User> {
+    const { username, password } = registerUserDto
+
+    const existingUser = await this.userModel.findOne({ username }).exec()
+    if (existingUser) {
+      throw new ConflictException('User already exists')
+    }
+
+    const salt = await bcrypt.genSalt()
+    const hashPassword = await bcrypt.hash(password, salt)
+
+    const newUser = new this.userModel({ username, password: hashPassword })
+    return newUser.save()
+  }
 
   async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((user) => user.username === username)
+    return this.userModel.findOne({ username }).exec()
+  }
+
+  async getUserProfile(userId: string): Promise<User> {
+    const user = await this.userModel.findById(userId).exec()
+    if (!user) throw new NotFoundException('User not found')
+
+    return user
   }
 }
