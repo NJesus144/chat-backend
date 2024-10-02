96f61c9b1b9b6e482bfdea0902ec989f50e10077
NJesus144 <nalbert-edm@outlook.com>
2024-10-02 08:16:49 -0300

    Refactor JwtStrategy to remove unused code and inject ConfigService

--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -1,16 +1,12 @@
 import { Strategy, ExtractJwt } from 'passport-jwt'
 import { PassportStrategy } from '@nestjs/passport'
 import { Injectable } from '@nestjs/common'
-import { AuthService } from '../auth.service'
 import { JwtPayload } from './jwt-payload.interface'
 import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private authService: AuthService,
-    private configService: ConfigService,
-  ) {
+  constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
