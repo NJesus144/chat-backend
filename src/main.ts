6e5fbd35520bd82c7760b230360d693d1afddc72
NJesus144 <nalbert-edm@outlook.com>
2024-10-10 20:31:09 -0300

    Refactor main.ts to add Swagger documentation and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { HttpExceptionFilter } from './common/filters/http-exception.filter'
+import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
   app.useGlobalFilters(new HttpExceptionFilter())
-  await app.listen(3001)
+  app.useGlobalPipes(new ValidationPipe())
+
+  const config = new DocumentBuilder()
+    .setTitle('Chat API')
+    .setDescription('Chat description')
+    .setVersion('1.0')
+    .addTag('API chat')
+    .addBearerAuth()
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(4000)
 }
 bootstrap()
